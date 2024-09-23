a46ea8838ccf80975989486838a17e2d36f116f3
EwanKM <starewan78@gmail.com>
2024-09-23 17:11:09 +0200

    exercice 2

--- TP2-algo.py
+++ TP2-algo.py
@@ -1,7 +1,7 @@
-from math import sqrt
-
-
-a = int(input("Entrez la valeur : "))
+"""
+Exercice 1  
+"""
+#a = int(input("Entrez la valeur : "))
 
 def premier(n):
     if n%2 == 0:
@@ -12,4 +12,17 @@ def premier(n):
     return True
 
 
-print(premier(a))
+#print(premier(a))
+"""
+Exercice 2
+"""
+n2 = 5
+nb_premiers = 0
+variable = 2
+liste = [2]
+while nb_premiers < n2-1 :
+    if premier(variable) == True :
+        liste.append(variable)
+        nb_premiers += 1
+    variable +=1
+print(liste)
