2eb148126bbd0157cb1dcf4b495cf5c50887136c
EwanKM <starewan78@gmail.com>
2024-09-24 13:37:37 +0200

    exercice 3

--- TP2-algo.py
+++ TP2-algo.py
@@ -1,7 +1,7 @@
 """
 Exercice 1  
 """
-#a = int(input("Entrez la valeur : "))
+a = int(input("Entrez la valeur : "))
 
 def premier(n):
     if n%2 == 0:
@@ -12,17 +12,47 @@ def premier(n):
     return True
 
 
-#print(premier(a))
+print(premier(a))
 """
 Exercice 2
 """
-n2 = 5
-nb_premiers = 0
 variable = 2
-liste = [2]
-while nb_premiers < n2-1 :
-    if premier(variable) == True :
-        liste.append(variable)
-        nb_premiers += 1
-    variable +=1
-print(liste)
+def exo2(variable):
+    n2 = int(input("Entrez le nombre que vous souhaitez : "))
+    nb_premiers = 0
+    liste = [variable]
+    while nb_premiers < n2-1 :
+        if premier(variable) == True :
+            liste.append(variable)
+            nb_premiers += 1
+        variable +=1
+    print(liste)
+exo2(variable)
+"""
+Exercice 3
+"""
+def exo3():
+    while True:
+        try:
+            borne_1 = int(input("borne 1 : "))
+            borne_2 = int(input("borne 2 : "))
+            
+            # Vérification si borne_1 est plus grande que borne_2
+            if borne_1 > borne_2:
+                print("Erreur : La première borne doit être inférieure ou égale à la deuxième. Veuillez réessayer.")
+            else:
+                break  # Si tout est correct, on sort de la boucle
+        except ValueError:
+            print("Erreur : Veuillez entrer des nombres valides. Réessayez.")
+    list3 = []
+    for loop in range(borne_1,borne_2):
+        if premier(loop) == True:
+            list3.append(loop)
+    print(list3)
+exo3()
+
+
+
+
+
+
